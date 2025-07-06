e37e869777f74e4e5ccdf23966068bf009def9e0
brayanroa <brayanroa38@gmail.com>
2025-07-06 12:35:25 -0500

    Elimina node_modules y dist del repositorio

--- dist/commands/list.js
+++
@@ -1,7 +0,0 @@
-import db from '../db/db.js';
-export const listProjects = async () => {
-    console.log('\n📁 Proyectos registrados:\n');
-    db.data.projects.forEach((project, index) => {
-        console.log(`${index + 1}. ${project.name} - ${project.path}`);
-    });
-};
--- dist/db/db.js
+++
@@ -1,8 +0,0 @@
-import { Low } from 'lowdb';
-import { JSONFile } from 'lowdb/node';
-const adapter = new JSONFile('projects.json');
-const defaultData = { projects: [] };
-const db = new Low(adapter, defaultData);
-await db.read();
-db.data || (db.data = defaultData);
-export default db;
